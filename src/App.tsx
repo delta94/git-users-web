cc17b4543960b77a6fbf86a8443aef765c3a803b
unknown <viniciusbeloti7@gmail.com>
2020-10-15 18:35:41 -0300

    chore: add filter to make search results

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React, { ChangeEvent, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import User from './components/User';
 import ResponseProps from './interfaces/ResponseProps';
 import api from './services/api';
 
 function App() {
   const [users, setUsers] = useState<ResponseProps[]>([]);
-  const [searchedUsers, setSearchedUsers] = useState<ResponseProps[]>([]);
   const [search, setSearch] = useState('');
   const [deletedUsers, setDeletedUsers] = useState<ResponseProps[]>([]);
   const [seeDeleted, setSeeleted] = useState(false);
@@ -14,7 +13,6 @@ function App() {
     (async function getUsers() {
       const response = await api.get('/users');
       setUsers(response.data);
-      // setOldUsers(response.data);
     })();
   }, []);
 
@@ -26,22 +24,29 @@ function App() {
     }
   };
 
-  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value);
+  const filterResults = React.useCallback(
+    (user) => {
+      if (
+        !search ||
+        user.login.includes(search) ||
+        String(user.id) === search
+      ) {
+        return true;
+      }
 
-    const searchExists = users.filter((user) => user.login.includes(search));
-
-
-  };
+      return false;
+    },
+    [search]
+  );
 
   return (
     <>
-      <input value={search} onChange={handleSearch} />
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
       <button onClick={handleSeeDeleted} type="submit">
         Ver Deletados
       </button>
 
-      {(seeDeleted ? deletedUsers : users).map((user) => (
+      {(seeDeleted ? deletedUsers : users).filter(filterResults).map((user) => (
         <User
           key={user.id}
           userId={user.id}
